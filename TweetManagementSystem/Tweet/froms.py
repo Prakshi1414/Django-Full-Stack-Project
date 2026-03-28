cb8e76a084017e2bf8e1a90569e79d1b7057ed35
Prakshi <prakshisolanki1414@gmail.com>
2026-03-28 13:44:31 +0530

    add jinja templates

--- TweetManagementSystem/Tweet/froms.py
+++
@@ -1,7 +0,0 @@
-from django import froms
-from .models import Tweet
-
-class TweetForm(froms.ModelForm):
-    class meta:
-        model = Tweet
-        fields = ['text', 'photo']
--- TweetManagementSystem/Tweet/views.py
+++ TweetManagementSystem/Tweet/views.py
@@ -1,22 +1,26 @@
 from django.shortcuts import render
 from .models import Tweet
-from .froms import TweetForm
+from .forms import TweetForm , UserregistrationForm
+from django.contrib.auth.decorators import login_required  #use for protected route
+from django.contrib.auth import login
+
 from django.shortcuts import get_object_or_404, redirect
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
-#list vall tweets in one page
+#list all tweets in one page
 def tweet_list(request):
    tweets= Tweet.objects.all().order_by('-created_at')
    return render(request, 'tweet_list.html', {'tweets':tweets})
 
+@login_required #create decorater so only login user an create tweet
 def tweet_create(request):
-    if request.method=='post':
-       form = TweetForm(request.Post ,request.FILES)
+    if request.method=='POST':
+       form = TweetForm(request.POST ,request.FILES)
        if form.is_valid():# only login user can create post
-         tweet = form.save(commite=False)
+         tweet = form.save(commit=False)
          tweet.user = request.user
          tweet.save()
          return redirect('tweet_list')
@@ -24,24 +28,40 @@ def tweet_create(request):
        form = TweetForm()
     return render(request, 'tweet_form.html', {'form':form})
 
+@login_required
 def tweet_edit(request , tweet_id):
     tweet = get_object_or_404(Tweet,pk=tweet_id, user=request.user)
-    if request.method=='post':
-       form = TweetForm(request.Post ,request.FILES, isinstance=tweet)
+    if request.method=='POST':
+       form = TweetForm(request.Post ,request.FILES,instance=tweet)
        if form.is_valid():# only login user can create post
-         tweet = form.save(commite=False)
+         tweet = form.save(commit=False)
          tweet.user = request.user
          tweet.save()
          return redirect('tweet_list')
     else:
-     form =  TweetForm(isinstance=tweet)
+     form =  TweetForm(instance=tweet)
      return render(request, 'tweet_form.html', {'form':form})
 
-
+@login_required
 def tweet_delete(request , tweet_id):
     tweet = get_object_or_404(Tweet, pk=tweet_id, user=request.user)
-    if request.method=='post':
+    if request.method=='POST':
          tweet.delete()
          return redirect('tweet_list')
     return render(request, 'tweet_confirm_delete.html', {'tweet':tweet})
+
+def register(request):
+    if request.method == 'POST':
+        form = UserregistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password1'])
+            user.save()
+            login(request, user)
+            return redirect('tweet_list')
+    else:
+        form = UserregistrationForm()   # yaha POST nahi dena
+
+    return render(request, 'registration/register.html', {'form': form})
+   
    
