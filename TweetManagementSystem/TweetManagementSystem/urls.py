76924d58e999313170f993ce3222060bd14c3806
Prakshi <prakshisolanki1414@gmail.com>
2026-03-28 14:31:46 +0530

    project complite search feature pending

--- TweetManagementSystem/TweetManagementSystem/urls.py
+++ TweetManagementSystem/TweetManagementSystem/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 from django.contrib.auth.urls import views as auth_views
 
 urlpatterns = [
+    path('', include('Tweet.urls')),
     path('admin/', admin.site.urls),
      path('tweet/', include('Tweet.urls')),
      path('accounts/', include('django.contrib.auth.urls')),
