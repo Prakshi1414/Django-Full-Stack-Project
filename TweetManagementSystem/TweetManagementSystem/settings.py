0e1648b1f0227057fa5aa51cedc24e0e96edfbf5
Prakshi <prakshisolanki1414@gmail.com>
2026-04-08 19:08:35 +0530

    provide storage for images

--- TweetManagementSystem/TweetManagementSystem/settings.py
+++ TweetManagementSystem/TweetManagementSystem/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 import os
+import cloudinary
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,7 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Tweet',
+    'cloudinary',
+    'cloudinary_storage',
 ]
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
